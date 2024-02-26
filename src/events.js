ae543f4c4d4a6132b4800521b969f0bfdfaf9b65
Nicolas Lopez <niclopezt@gmail.com>
2024-02-26 17:06:41 +1100

    Hide the dropdown menu when clicking out of it

--- src/events.js
+++ src/events.js
@@ -5,8 +5,24 @@
 export function activateEvents() {
     const dropdownIcon = document.getElementById("dropdown_menu");
     dropdownIcon.addEventListener("click", toggleMenu);
+
+    closeDropdownMenu();
 }
 
 function toggleMenu() {
+    /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
     document.getElementById("entries_menu").classList.toggle("hidden");
+}
+
+function closeDropdownMenu() {
+    // Close the dropdown if the user clicks outside of it
+
+    window.onclick = function(event) {
+        if (!event.target.matches(".dropdown-icon")) {
+            const menu = document.getElementById("entries_menu");
+    
+            if (!menu.classList.contains("hidden")) menu.classList.add("hidden");
+        }
+    }
 }
